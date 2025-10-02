6a3c285d21a836ac91ce10162fc2e20e14aa594c
jose Angel <josegallardo.cor@gmail.com>
2025-10-01 21:16:13 -0600

    feat: separacion en componentes de GifsApp

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,42 +1,21 @@
+import { GifList } from "./gifs/components/GifList"
+import { PreviousSearches } from "./gifs/components/PreviousSearches"
 import { mockGifs } from "./mock-data/gifs.mock"
+import { CustomHeader } from "./shared/components/CustomHeader"
+import { SearchBar } from "./shared/components/SearchBar"
 
 
 export const GifsApp = () => {
   return (
     <>
     {/* Header */}
-    <div className="content-center">
-        <h1>Buscador de Gifs</h1>
-        <p>Descubre y commparte el gif perfecto</p>
-    </div>
+    <CustomHeader title="Buscador de Gifs"  description="Descubre y comparte el Gif perfecto"/>
     {/* Search */}
-    <div className="search-container">
-        <input type="text" placeholder="Buscar gifs..." />
-        <button>Buscar</button>
-    </div>
+    <SearchBar placeholder="Busca lo que quieras"/>
     {/* Busquedas previas */}
-    <div className="previous-searches">
-        <h2>Busquedas previas</h2>
-        <ul className="previous-searches-list">
-            <li>Goku</li>
-            <li>Saitama</li>
-            <li>Elden Ring</li>
-        </ul>
-    </div>
+    <PreviousSearches searches={['Goku', 'Dragon Ball Z', 'Naruto']} />
     {/* Gisf */}
-    <div className="gifs-container">
-        {
-            mockGifs.map( (gif) => (
-                <div key={gif.id} className="gif-card" >
-                    <img src={gif.url} alt={gif.title}/>
-                    <h3>{gif.title}</h3>
-                    <p>
-                        {gif.width}x{gif.height} (1.5mb)
-                    </p>
-                </div>
-            ))
-        }
-    </div>
+    <GifList gifs={mockGifs} />
     </>
   )
 }
---
+++ src/shared/components/SearchBar.tsx
@@ -0,0 +1,13 @@
+interface Props {
+    placeholder?: string;
+}
+
+
+export const SearchBar = ({placeholder = 'Buscar'}: Props) => {
+  return (
+    <div className="search-container">
+        <input type="text" placeholder={placeholder} />
+        <button>Buscar</button>
+    </div>
+  )
+}
