93560933989d0821d2c74c2b04235875bf2beb3e
jose Angel <josegallardo.cor@gmail.com>
2025-10-02 20:24:15 -0600

    feat: busqueda de gifs y visualizacion

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -4,22 +4,27 @@ import { PreviousSearches } from "./gifs/components/PreviousSearches"
 import { mockGifs } from "./mock-data/gifs.mock"
 import { CustomHeader } from "./shared/components/CustomHeader"
 import { SearchBar } from "./shared/components/SearchBar"
+import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action"
+import type { Gif } from "./gifs/interfaces/gif.interface"
 
 
 export const GifsApp = () => {
-    const [previousTerms, setPreviousTerms] = useState(['dragon ball z'])
+    const [previousTerms, setPreviousTerms] = useState<string[]>([]);
+    const [gifsList, setGifsList] = useState<Gif[]>([]);
     const handleTermClicked = (term: string) => {
         console.log({term})
 
     };
-    const handleSearch = (query: string = '') => {
+    const handleSearch = async (query: string = '') => {
       const term = query.trim().toLowerCase();
       if(term.length === 0) return;
       if(previousTerms.includes(term)) return;
       setPreviousTerms((prev) => {
         const newTerms = [term, ...prev];
         return newTerms.slice(0,6)
-      })
+      });
+      const gifs = await getGifsByQuery(term);
+      setGifsList(gifs);
     }
   return (
     <>
@@ -30,7 +35,7 @@ export const GifsApp = () => {
     {/* Busquedas previas */}
     <PreviousSearches searches={previousTerms} onLabelClicked={handleTermClicked}/>
     {/* Gisf */}
-    <GifList gifs={mockGifs} />
+    <GifList gifs={gifsList} />
     </>
   )
 }
