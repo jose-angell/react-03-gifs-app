0657c17bc8892ec51f6125a65272e100724b9038
jose Angel <josegallardo.cor@gmail.com>
2025-10-05 21:27:25 -0600

    feat: control de cache

--- src/gifs/hooks/useGifs.tsx
+++ src/gifs/hooks/useGifs.tsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import type { Gif } from "../interfaces/gif.interface";
 import { getGifsByQuery } from "../actions/get-gifs-by-query.action";
 
 
+//const gifsCache: Record<string,Gif[]> = {};
 
 export const useGifs = () => {
     const [previousTerms, setPreviousTerms] = useState<string[]>([]);
     const [gifsList, setGifsList] = useState<Gif[]>([]);
+    const gifsCache = useRef<Record<string,Gif[]>>({});  
     const handleTermClicked = async (term: string) => {
+       if(gifsCache.current[term]){
+        setGifsList(gifsCache.current[term]);
+        return;
+       }
        const gifs = await getGifsByQuery(term);
        setGifsList(gifs);
     };
@@ -19,8 +25,11 @@ export const useGifs = () => {
         const newTerms = [term, ...prev];
         return newTerms.slice(0,6)
       });
+      
       const gifs = await getGifsByQuery(term);
       setGifsList(gifs);
+
+      gifsCache.current[term] = gifs;
     }
   return {
     gifsList,
